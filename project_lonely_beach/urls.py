7360fcc67c73b8ed2ee2f9c7c826020123b75035
Rakdoslover <>
2023-08-09 08:05:23 +0000

    added 32x32 and 16x16 for the png pic i want for the favicon

--- project_lonely_beach/urls.py
+++ project_lonely_beach/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.templatetags.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(
+        url=static('img/favicon.ico'))),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('book.urls'), name='book_urls'),
