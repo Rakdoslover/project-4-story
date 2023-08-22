17558473b37f55c6997684546680b5a3b358ef2b
Oscar Lundsör <oscarlundsor@gmail.com>
2023-08-22 18:26:10 +0000

    added whitenoise to collect the css

--- project_lonely_beach/settings.py
+++ project_lonely_beach/settings.py
@@ -86,6 +86,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
